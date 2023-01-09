01e050b823e16fafc7b8026cb520058f8c4f650d
beOk91 <be_ok91@naver.com>
2023-01-09 14:31:40 +0900

    first commit

---
+++ src/test/java/com/devops/cj/CjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.cj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
